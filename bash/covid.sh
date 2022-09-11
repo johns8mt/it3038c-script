5e1474655d4eda9b35bd21827b2e5fed3a132602
johns8mt <64618521+johns8mt@users.noreply.github.com>
2022-09-11 14:49:30 -0400

    Update covid.sh

--- bash/covid.sh
+++ bash/covid.sh
@@ -8,6 +8,6 @@ NEGATIVE=$(echo $DATA | jq '.[0].negative')
 DEATHS=$(echo $DATA | jq ' .[0].death')
 HOSPITALIZED=$(echo $DATA | jq ' .[0].hospitalized')
 
-echo "On $TODAY, there were $POSITIVE positive COVID cases, there were $NEGATIVE negative COVID tests taken, there have also been $DEATHS people die. There are still $HOSPITALIZED people still hospitalized, please be sure to get vaccinated and continue to praice CDC guidelines so we can continue to fight COVID together!"
+echo "On $TODAY, there were $POSITIVE positive COVID cases, there were $NEGATIVE negative COVID tests taken, there have also been $DEATHS people die. There are still $HOSPITALIZED people still hospitalized, please be sure to get vaccinated and continue to practice CDC guidelines so we can continue to fight COVID together!"
 
 
