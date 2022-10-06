3207bf3caa8bdf656c4e77feb2ccf9c4d8379fe1
johns8mt <64618521+johns8mt@users.noreply.github.com>
2022-10-06 18:16:41 -0400

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -13,3 +13,7 @@ print("The date today is...", today)
 
 #I tried multiple diifferent ways to get the date to work but found the today=date.today() method from https://www.programiz.com/python-programming/datetime/current-datetime
 #To change up the code I added the time as well.
+#1. Goal of the script is to display the current date and time using python. 
+#2. To run the script simply copy the script and run iin any IDE that allows python. 
+#3. The output of the script should be similar to this (will change depending on date and time): 
+#The current time is... 22:09:42 The date today is... 2022-10-06
