e4ed2659ab86195a8fd70d433b8290a872565a42
johns8mt <64618521+johns8mt@users.noreply.github.com>
2022-10-09 21:49:04 -0400

    Update Lab6.js

--- Labs/Lab6.js
+++ Labs/Lab6.js
@@ -1,14 +1,27 @@
-var osutils = require("os-utils");
+var http = require("http")
+var fs = require("fs")
+var os = require("os")
+var ip = require("ip")
 
-console.log("Platform: " + osutils.platform());
-console.log("Number of CPUs: " + osutils.cpuCount());
+name = os.hostname;
 
-osutils.cpuUsage(function(v) {
-  console.log("CPU Usage (%) : " + v);
-});
 
-console.log("Total Memory: " + osutils.totalmem() + "MB");
+uptime = os.uptime
+uptimeDays = Math.floor (uptime / 86400)
+uptime = uptime - (uptimeDays * 86400);
+upHours = Math.floor (uptime / 3600);
+uptime = uptime - (upHours * 3600);
+upMins = Math.floor (uptime / 60);
+uptime = uptime - (upMins *60)
+upSecs = Math.floor (uptime);
 
-console.log("Free Memory: " + osutils.freemem() + "MB");
+cpus = os.cpus(),
+cpuNumber = cpus.length;
+Memory = os.totalmem;
+FreeMemory = os.freemem;
+TotalMemory = Memory / 1000000;
+FreeMemory = FreeMemory / 1000000;
 
-console.log("System Uptime: " + osutils.sysUptime() + "ms");
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {"Content-Type":"text/html"});
+    res.end(
