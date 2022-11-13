2847da2ca1196d8686c9d8d0b5a4d66a8b741d8a
johns8mt <64618521+johns8mt@users.noreply.github.com>
2022-11-13 15:25:48 -0500

    Update and rename lab9api.js to lab9.py

---
+++ Labs/lab9.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+
+r = requests.get('http://localhost:3000')
+
+data = r.json()
+
+for i in data:
+    print(i['name'] + " is " + i['color'])
+
+input ("Press any key")
